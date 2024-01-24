7ccd62f802a9eda6cd024c567c405b12265fb6d1
YunDoll <20201106@vision.hoseo.edu>
2024-01-24 11:39:44 +0900

    01-24 front set

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:ir_news/app_bar.dart';
+import 'package:ir_news/pages/home_page.dart';
+import 'package:ir_news/pages/category_page.dart';
+import 'package:ir_news/pages/scrap_page.dart';
+import 'package:ir_news/pages/mypage_page.dart';
+import 'package:ir_news/pages/license_page.dart' as UserLicensePage;
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NARA',
+      debugShowCheckedModeBanner: false, // 디버그 라벨 제거
+      theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white, // 기본 색상
+          )),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MainPage(),
+        '/license': (context) => UserLicensePage.LicensePage(),
+      },
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 1;
+
+  final List<Widget> _pages = [ScrapPage(), HomePage(), CategoryPage(), MypagePage()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BaseAppBar(),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border),
+            label: '스크랩',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.newspaper),
+            label: '카테고리',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '마이페이지',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Color.fromARGB(255, 252, 220, 1),
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
